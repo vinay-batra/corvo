74872ce2c61fb0ce72f1aa4ee467a5f1f1f7028c
vinay-batra <vinaybatra2010@gmail.com>
2026-04-20 16:43:32 -0400

    fix: html/body height auto, body bg #0a0e14, ParticleCanvas 100% coverage

--- frontend/components/ParticleCanvas.tsx
+++ frontend/components/ParticleCanvas.tsx
@@ -64,10 +64,11 @@ export default function ParticleCanvas() {
         position: "fixed",
         top: 0,
         left: 0,
-        width: "100vw",
-        height: "100vh",
+        width: "100%",
+        height: "100%",
         pointerEvents: "none",
         zIndex: 0,
+        background: "#0a0e14",
       }}
     />
   );
