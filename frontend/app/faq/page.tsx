99749b71a499fc78c40a623c715ecc0854fe5e03
vinay-batra <vinaybatra2010@gmail.com>
2026-04-20 16:56:59 -0400

    fix: remove duplicate React import in faq page

--- frontend/app/faq/page.tsx
+++ frontend/app/faq/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from "react";
-import React from "react";
 import PublicNav from "@/components/PublicNav";
 import PublicFooter from "@/components/PublicFooter";
 
